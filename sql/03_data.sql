0bf791decfed9863fb9aea95c5ee9a86b78ec558
gitrust <weebac@web.de>
2019-02-28 22:52:09 +0100

    new sql sample data

---
+++ sql/03_data.sql
@@ -0,0 +1,29 @@
+
+
+insert into ActivityCategories(name,description) VALUES ("Gottesdienst","Description Dptm 1");
+insert into ActivityCategories(name,description) VALUES ("Tontechnik","Description Dptm 2");
+insert into ActivityCategories(name,description) VALUES ("Lobpreis","Description Dptm 2");
+insert into ActivityCategories(name,description) VALUES ("Kinderdienst","KiDi");
+
+insert into Activities (categoryId,name,description) VALUES (5,"Leitung","Description");
+insert into Activities (categoryId,name,description) VALUES (5,"E-Gitarre","Description");
+insert into Activities (categoryId,name,description) VALUES (5,"Kachon","Description");
+insert into Activities (categoryId,name,description) VALUES (5,"Piano","Description");
+insert into Activities (categoryId,name,description) VALUES (5,"Sänger 1","Description");
+insert into Activities (categoryId,name,description) VALUES (5,"Sänger 2","Description");
+
+insert into Activities (categoryId,name,description) VALUES (3,"Prediger","Description");
+insert into Activities (categoryId,name,description) VALUES (3,"Ordner 1","Description");
+insert into Activities (categoryId,name,description) VALUES (3,"Ordner 2","Description");
+insert into Activities (categoryId,name,description) VALUES (3,"Leitung","Description");
+insert into Activities (categoryId,name,description) VALUES (3,"Info-Tresen","Description");
+insert into Activities (categoryId,name,description) VALUES (3,"Welcome 1","Description");
+insert into Activities (categoryId,name,description) VALUES (3,"Welcome 2","Description");
+
+insert into Activities (categoryId,name,description) VALUES (4,"Beamer","Description");
+insert into Activities (categoryId,name,description) VALUES (4,"Mischpult 1","Description");
+insert into Activities (categoryId,name,description) VALUES (4,"Mischpult 2","Description");
+
+insert into Activities (categoryId,name,description) VALUES (6,"Schäffchen","Description");
+insert into Activities (categoryId,name,description) VALUES (6,"Bibelkreis","Description");
+insert into Activities (categoryId,name,description) VALUES (6,"Kids ab 7","Description");
