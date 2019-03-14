f7e8f85803dabd0bc63e24024e1a4dadf67d92a1
gitrust <weebac@web.de>
2019-03-14 21:24:39 +0100

    Use ini file for i18n

--- public/helpers/i18n.php
+++ public/helpers/i18n.php
@@ -2,73 +2,15 @@
 
 class I18n {
     
-	private static  $msg = 
-		array("button.save" => "Speichern",
-			"label.administrator" => "Administrator",
-			"table.header.admin" => "Administrator",
-			"table.header.firstname" => "Name",
-			"table.header.email" => "Email",
-			"label.password" => "Passwort",
-			"label.firstname" => "Name",
-			"label.email" => "Email",
-			"title.login" => "Anmelden",
-			"link.administration" => "Administration",
-			"link.logout" => "Abmelden",
-			"link.entries" => "Dienstplan",
-			"link.roles" => "Dienste",
-			"link.users" => "Benutzer",
-			"link.schedules" => "Termine",
-			"link.mylist" => "Meine Termine",
-			"link.teamlist" => "Team Uebersicht",
-			"table.noentries" => "Derzeit gibt es keine Eintraege!",
-			"form.login" => "Anmelden",
-			"form.password" => "Passwort",
-			"button.login" => "Anmelden",
-			"link.imprint" => "Impressum",
-			"link.privacy" => "Datenschutz",
-			"title.entrylist" => "Dienstplan",
-			"title.welcome" => "Willkommen",
-			"button.update" => "Aktualisieren",
-			"table.header.entrylist" => "Termine / Dienste",
-			"title.teamlist" => "Team Uebersicht",
-			"table.header.roles" => "Dienste",
-			"link.edit" => "...",
-			"link.delete" => "x",
-			"title.rolesite" => "Dienste",
-			"label.newrole" => "Neuer Dienst",
-			"button.add" => "ADD",
-			"link.activities" => "Aktivitaeten",
-			"link.resources" => "Ressourcen",
-			"table.header.schedules" => "Termine",
-			"label.newschedule" => "Neuer Termin",
-			"label.scheduledescription" => "Beschreibung",
-			"label.roledescription" => "Beschreibung",
-			"label.userlogin" => "Benutzer Login",
-			"label.userfirstname" => "Name",
-			"label.userpass" => "Passwort",
-			"label.userrole" => "Systemrolle",
-			"table.header.users" => "Benutzer",
-			"label.newuser" => "Neuer Benutzer",
-			"title.users" => "Benutzer",
-			"title.schedules" => "Termine",
-			"label.category" => "Kategorie",
-			"label.description" => "Beschreibung",
-			"label.newactivity" => "Neue Aktivität",
-			"table.header.activities" => "Aktivitäten",
-			"table.header.resources" => "Ressourcen",
-			"label.newresource" => "Neue Ressource",
-			"title.resourcesite" => "Ressourcen",
-			"title.activitysite" => "Aktivitäten",
-			"label.user" => "Benutzer",
-			"label.manager" => "Manager",
-			"title.chooseresource" => "Ressource wählen",
-			"option.chooseresource" => "Ressource wählen",
-			"title.privacy" => "Datenschutz",
-			"title.imprint" => "Impressum"
-			);
+	private static  $msg = null;
 	
-   public static function tr($msgid, $lang = "de") {
-   	 if (array_key_exists($msgid, self::$msg)){
+   public static function tr($msgid, $lang = "de-de") {
+     // currently only first requested language is set
+     if (self::$msg == null) {
+        self::$msg = parse_ini_file("static/i18n/" . $lang . ".ini");
+     }
+     
+     if (array_key_exists($msgid, self::$msg)){
        return self::$msg[$msgid];
      }
      return $msgid;
