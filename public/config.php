91f1af45816c73dc9bf3471e11f2af0132dd258b
gitrust <weebac@web.de>
2019-03-21 22:13:58 +0100

    Change site configuration

--- public/config.php
+++ public/config.php
@@ -4,32 +4,30 @@
 date_default_timezone_set('Europe/Berlin');
 
 //site address
-define('ENVIRONMENT', 'production');
+define('ENVIRONMENT', 'development');
+
 
 define('DOCROOT', dirname(__FILE__));
 
 // Credentials for the local server
-//define('DIR','http://localhost:8080/');
-//define('DB_TYPE','mysql');
-//define('DB_HOST','db');
-//define('DB_NAME','wteamplaner');
-//define('DB_USER','wteamplaner');
-//define('DB_PASS','wteamplaner');
+define('DIR','http://localhost:8080/');
+define('DB_TYPE','mysql');
+define('DB_HOST','db');
+define('DB_NAME','resourceplanner');
+define('DB_USER','resourceplanner');
+define('DB_PASS','resourceplanner');
 
-// all-inkl
- define('DIR','https://wtplaner.immanuel-kf.de/');
- define('DB_TYPE','mysql');
- define('DB_HOST','localhost');
- define('DB_NAME','d02b38d7');
- define('DB_USER','d02b38d7');
- define('DB_PASS','f6prfA8woUbcW3u6');
 //define('DB_SQL_SECRET_KEY','hdsfiiDsdfLkk27');
 
 //set prefix for sessions
-define('SESSION_PREFIX','wteam_');
+define('SESSION_PREFIX','rplan_');
 
 //optionall create a constant for the name of the site
-define('SITETITLE','WTeamPlaner');
+define('SITETITLE','Resourcen Planner');
+
+// site language
+// put new translation to static/i18n/*
+define('SITELANGUAGE','en-us');
 
 // session timeout in minutes
 define('SESSION_TIMEOUT',10);
