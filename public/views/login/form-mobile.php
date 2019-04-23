5b9dff5b7156545cae652f233fc9cee4bb77f30a
gitrust <weebac@web.de>
2019-04-23 13:26:41 +0200

    New login style

--- public/views/login/form-mobile.php
+++ public/views/login/form-mobile.php
@@ -1,3 +1,4 @@
+<div class="loginmain">
 
 <h1><?= I18n::tr('title.site');  ?></h1>
 
@@ -8,40 +9,18 @@
 ?>
   
 <!-- The above form looks like this -->
-<form method="POST" action="<?= DIR ?>login/login">
-  <input type="hidden" name="mobile" value="1">
-  <div class="row">
-    <div class="four columns">&nbsp;
-    </div>
-    <div class="four columns">
-      <label for="loginInput"><?= I18n::tr('form.login'); ?></label>
-      <input class="u-full-width" placeholder="login" name="login" id="loginInput" type="text">
-    </div>
-    <div class="four columns">&nbsp;
-    </div>    
-  </div>
-  
-  <div class="row">
-    <div class="four columns">&nbsp;
-    </div>
-    <div class="four columns">
-      <label for="pswInput"><?= I18n::tr('form.password'); ?></label>
-      <input class="u-full-width" placeholder="*****" id="pswInput" type="password" name="pass">
-    </div>
-    <div class="four columns">&nbsp;
-    </div>
-  </div>
-  
-  <div class="row">
-    <div class="four columns">&nbsp;
-    </div>
-    <div class="two columns">
-      <input class="button-primary" value="<?= I18n::tr('button.login'); ?>" type="submit">
-      
-    </div>
-    <div class="two columns"><a href="/login">Desktop Version</a>
-    </div>
-    <div class="four columns">&nbsp;
-    </div>
-  </div>
-</form>
+<div class="login-box">
+
+    <img src="/static/img/avatar.png" class="avatar">
+        <h1><?= I18n::tr('form.login'); ?></h1>
+        <form method="POST" action="<?= DIR ?>login/login/">
+            <p><?= I18n::tr('form.login'); ?></p>
+            <input type="text" name="login" class="logininput" placeholder="Enter Username" required>
+            <p><?= I18n::tr('form.password'); ?></p>
+            <input type="password" name="pass" class="logininput" placeholder="Enter Password" required>
+            <input type="submit" name="submit"  value="<?= I18n::tr('button.login'); ?>">
+            <a href="/login">Desktop Version</a> 
+        </form>
+</div>
+
+</div>
