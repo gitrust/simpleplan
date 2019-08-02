c2741b5cb40693c88cd28f34ece7f2c5310a5a42
gitrust <weebac@web.de>
2019-08-02 23:57:15 +0200

    Format users controller code

--- public/controllers/users.php
+++ public/controllers/users.php
@@ -16,67 +16,74 @@ class Users extends Controller {
   }
 
  /** API: Get Users */
- public function users() {  
-  $data['users'] = $this->_model->users(Session::get('userid'));
-  $data['title'] = I18n::tr('title.users') ;
-  $data['form_header'] = I18n::tr('form.login');
-  $this->renderUsers($data);    
-}
+  public function users() {  
+    $data['users'] = $this->_model->users(Session::get('userid'));
+    $data['title'] = I18n::tr('title.users') ;
+    $data['form_header'] = I18n::tr('form.login');
+
+    $this->renderUsers($data);    
+  }
 
 /** API: Delete User */
-public function del($id) {
-  if (!empty($id)) {
-    $this->_model->deleteUser($id,Session::get('userid'));
+  public function del($id) {
+    if (!empty($id)) {
+      $this->_model->deleteUser($id,Session::get('userid'));
+    }
+
+    $data['users'] = $this->_model->users(Session::get('userid'));
+    $data['title'] = I18n::tr('title.users') ;
+    $data['form_header'] = I18n::tr('form.login');
+    
+    $this->renderUsers($data);    
   }
 
-  $data['users'] = $this->_model->users(Session::get('userid'));
-  $data['title'] = I18n::tr('title.users') ;
-  $data['form_header'] = I18n::tr('form.login');
-  $this->renderUsers($data);    
-}
+  /** API: Add New User */
+  public function add() {
+    $this->addUser();
 
-/** API: Add New User */
-public function add() {
-  $this->addUser();
-
-  $data['users'] = $this->_model->users(Session::get('userid'));
-  $data['title'] = I18n::tr('title.users') ;
-  $data['form_header'] = I18n::tr('form.login');
-  
-  $this->renderUsers($data); 
-}
-
- // Helper Function
- private function addUser() {
-  if (!empty($_POST['login']) && !empty($_POST['firstname']) && !empty($_POST['pass']) && !empty($_POST['userrole'])) {
-      $genpass = Pass::generate($_POST['pass']);
-      $email = $_POST['email'];
-      $userRole = $_POST['userrole'];
-      $description = $_POST['description'];
-      $this->_model->addUser(trim($_POST['login']), trim($_POST['firstname']), $genpass, $email, $userRole, $description);
+    $data['users'] = $this->_model->users(Session::get('userid'));
+    $data['title'] = I18n::tr('title.users') ;
+    $data['form_header'] = I18n::tr('form.login');
+    
+    $this->renderUsers($data); 
   }
-} 
+
+  // Helper Function
+  private function addUser() {
+    if (!empty($_POST['login']) 
+      && !empty($_POST['firstname']) 
+      && !empty($_POST['pass']) 
+      && !empty($_POST['userrole'])) {
+
+        $genpass = Pass::generate($_POST['pass']);
+        $email = $_POST['email'];
+        $userRole = $_POST['userrole'];
+        $description = $_POST['description'];
+
+        $this->_model->addUser(trim($_POST['login']), trim($_POST['firstname']), $genpass, $email, $userRole, $description);
+    }
+  } 
 
 
-private function renderUsers($data) {
-  $data["isadmin"] = $this->isAdmin();
-  $data["ismanager"] = $this->isManager();
-  $ismobile = Session::get('ismobileversion');
+  private function renderUsers($data) {
+    $data["isadmin"] = $this->isAdmin();
+    $data["ismanager"] = $this->isManager();
+    $ismobile = Session::get('ismobileversion');
 
-  $this->_view->render('header', $data);
-  $this->_view->render('container_start', $data);
-  $this->_view->render('nav', $data);
-  $this->_view->render('partials/admin/head', $data);
-  $this->_view->render('partials/admin/nav', $data);
-  $this->_view->render('users/edit', $data);
-  if ($ismobile) {
-    $this->_view->render('users/table_mobile', $data);
-  } else {
-    $this->_view->render('users/table', $data);
-  }
-  $this->_view->render('partials/admin/footer', $data);
-  $this->_view->render('container_end', $data);
-  $this->_view->render('footer');
-} 
+    $this->_view->render('header', $data);
+    $this->_view->render('container_start', $data);
+    $this->_view->render('nav', $data);
+    $this->_view->render('partials/admin/head', $data);
+    $this->_view->render('partials/admin/nav', $data);
+    $this->_view->render('users/edit', $data);
+    if ($ismobile) {
+      $this->_view->render('users/table_mobile', $data);
+    } else {
+      $this->_view->render('users/table', $data);
+    }
+    $this->_view->render('partials/admin/footer', $data);
+    $this->_view->render('container_end', $data);
+    $this->_view->render('footer');
+  } 
 
 }
