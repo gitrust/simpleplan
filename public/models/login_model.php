a5f2f1398c57847047efb3faec7b5d548092c09d
gitrust <weebac@web.de>
2019-08-15 22:06:39 +0200

    Select specific fields in SQL (login model)

--- public/models/login_model.php
+++ public/models/login_model.php
@@ -2,17 +2,18 @@
 
 class Login_Model extends Model {
 
-  public function __construct(){
-    parent::__construct();
-  }
+    public function __construct(){
+        parent::__construct();
+    }
 
-  public function usersByLogin($login,$pass) {
-    return  $this->_db->select('SELECT * FROM Users WHERE login = :login ORDER BY id DESC LIMIT 0, 1',array("login" => $login));       
-  }
+    public function usersByLogin($login,$pass) {
+        return  $this->_db->select('SELECT id,firstname,pass FROM Users WHERE login = :login ORDER BY id DESC LIMIT 0, 1',
+            array("login" => $login));       
+    }
 
-  public function updateLastLogin($login) {
-    $timestamp = date('Y-m-d G:i:s');
-    return  $this->_db->update('Users',array('lastlogin' => $timestamp),array('login' => $login));
-  }
+    public function updateLastLogin($login) {
+        $timestamp = date('Y-m-d G:i:s');
+        return  $this->_db->update('Users',array('lastlogin' => $timestamp),array('login' => $login));
+    }
 
 }
